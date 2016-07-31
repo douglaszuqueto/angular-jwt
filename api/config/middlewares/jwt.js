543d8af3cd53d9679dd52d32acef1f7d9ff84fff
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2016-07-31 16:28:02 -0300

    Refactoring jwt middleware and run project using nodemon;

--- api/config/middlewares/jwt.js
+++ api/config/middlewares/jwt.js
@@ -5,24 +5,7 @@ module.exports = (req, res, next) => {
     // check header or url parameters or post parameters for token
     let token = req.body.token || req.query.token || req.headers['authorization'];
 
-    // decode token
-    if (token) {
-
-        // verifies secret and checks exp
-        jwt.verify(token, secret, (err, decoded) => {
-            if (err) {
-                return res.status(403).send({
-                    success: false, message: 'Failed to authenticate token.'
-                });
-            } else {
-                // if everything is good, save to request for use in other routes
-                req.decoded = decoded;
-                next();
-            }
-        });
-
-    } else {
-
+    if (!token) {
         // if there is no token
         // return an error
         return res.status(403).send({
@@ -31,4 +14,17 @@ module.exports = (req, res, next) => {
         });
 
     }
+    // verifies secret and checks exp
+    jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({
+                success: false,
+                message: 'Failed to authenticate token.'
+            });
+        } else {
+            // if everything is good, save to request for use in other routes
+            req.decoded = decoded;
+            next();
+        }
+    });
 };
